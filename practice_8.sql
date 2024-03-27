0cf9f3ff0998d76bd83d7cb289809a542bbafcf7
tragiangho <150886212+tragiangho@users.noreply.github.com>
2024-03-27 14:00:08 +0700

    Update practice_8.sql

--- practice_8.sql
+++ practice_8.sql
@@ -35,3 +35,37 @@ INNER JOIN
 ON DATE_PART('day',visited_on) - DATE_PART('day', min_visited_on) >= 6
 order by visited_on asc
 --ex5
+with cte_1 as
+(select pid, tiv_2015, tiv_2016,
+concat (lat,' ', lon) as lat_lon
+from Insurance)
+select sum (tiv_2016) as tiv_2016
+from cte_1
+where tiv_2015 in (select tiv_2015 from cte_1 group by tiv_2015 having count (*)>1 )
+and lat_lon in (select lat_lon from cte_1 group by lat_lon having count (*)=1 )
+--ex6
+with cte_1 as
+(select a.name as Employee, a.Salary, b.name as Department
+from Employee as a
+inner join Department as b on a.departmentId=b.id)
+select Department,Employee, Salary from 
+(select Department,Employee, Salary,
+dense_rank() over (partition by department order by salary desc) as rk
+from cte_1) as cte_2
+where rk<=3
+--ex7
+with Queue_table as 
+(select turn, person_id, person_name, weight,
+sum(weight)over(order by turn) as total_weight
+from Queue
+order by turn)
+select person_name from 
+Queue_table
+where total_weight=1000
+--ex8
+select distinct product_id, 10 as price from Products where product_id not in 
+(select distinct product_id from Products where change_date <='2019-08-16' )
+union
+select product_id, new_price as price from Products 
+where (product_id,change_date) in (select product_id , max (change_date) as date from Products where change_date <='2019-08-16' 
+group by product_id)
